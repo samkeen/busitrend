6bd96a313ba7e46472b4e9afdf577a082e5b8e8a
samkeen <sam.keen@nike.com>
2014-10-16 07:19:12 -0700

    added CORS headers

--- app.js
+++ app.js
@@ -10,14 +10,26 @@ var business_lics = require('./routes/business_lics');
 
 var app = express();
 
+// #todo, move this to config or simply open to '*'
+var clientHost = 'http://localhost:4000';
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.all("/api/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", clientHost);
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    return next();
+});
+
 app.use('/', routes);
 app.use('/api/business_lics', business_lics);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
